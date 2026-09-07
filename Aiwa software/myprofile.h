90d4f8a65c75c72ee837dcb936bbe99f95f01cae
Amagami45 <bedrichzimmermann597@gmail.com>
2026-09-07 18:34:52 +0200

    Add files via upload

---
+++ Aiwa software/myprofile.h
@@ -0,0 +1,23 @@
+#ifndef myprofile_h
+#define myprofile_h
+
+#define DSP_MODEL DSP_ILI9341
+#define CS_PIN 15
+#define DC_PIN 2
+#define RST_PIN 4
+
+#define I2S_DOUT 27
+#define I2S_BCLK 26
+#define I2S_LRC 25
+
+#define ENCODER_PIN_A 32
+#define ENCODER_PIN_B 33
+#define ENCODER_BTN 34
+
+
+#define BTN_UP 22    
+#define BTN_DOWN 21  
+#define BTN_LEFT 19
+#define BTN_RIGHT 18
+
+#endif
